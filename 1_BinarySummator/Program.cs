2362223a8b633d2ece7cb7380a43280a2b63331d
Dmytro Kyba <dkyba2006@gmail.com>
2024-03-14 12:07:27 +0200

    done the report

--- 1_BinarySummator/Program.cs
+++ 1_BinarySummator/Program.cs
@@ -73,7 +73,7 @@ namespace BinarySummator
     {
       for (int i = 0; i < binary.Length; i++)
       {
-        if (binary.Length != 7)//hardcode
+        if (binary.Length != 7) //hardcode
         {
           Console.WriteLine("That length is not 7");
           return false;
